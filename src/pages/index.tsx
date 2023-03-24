9db7261698951785d8f42ab2ca553f411282cb4e
Yegor Arndt <devegorarndt@gmail.com>
2023-03-24 17:12:25 +0400

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,9 @@ import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { api } from "y/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -18,6 +18,11 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         {user.isSignedIn ? <SignOutButton /> : <SignInButton />}
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
